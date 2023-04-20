3aedaba184c9935bbc10610f0721bb67b1702d97
Devmcee <info@devmc.ee>
2023-04-20 23:37:41 +0300

    add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html>
       <Head />
       <body>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-59CSMT6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        >
+          {/* <!-- End Google Tag Manager (noscript) --> */}
+        </noscript>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
