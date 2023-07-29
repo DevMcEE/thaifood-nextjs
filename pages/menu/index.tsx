d25f859b9fdd32539575a42301cbf2f207c1040a
“Roman <lavrunov@gmail.com>
2023-07-29 10:41:52 +0300

    TE-11: Add toolbar, configure menu page layout

--- pages/menu/index.tsx
+++ pages/menu/index.tsx
@@ -8,48 +8,48 @@ interface MenuPageProps {
   menu: MenuGroup[];
 }
 
-export default function Menu({menu}:MenuPageProps) {
-  console.log({menu});
+export default function Menu({ menu }: MenuPageProps) {
   return (
     <>
-    <Meta />
-    <Toolbar />
-    <div className="body-container">
-      <main className="content-container main-page__content">
-        <div className="content-block">
-          <div className="content-block__title">
-            <h2>Menu</h2>
-          </div>
-          <div className="content-block__content">{
-            menu.map(({name,items,_id})=> {
-              return (<div key={_id}>
-                <h3>{name}</h3>
-                <div>
-                  {items.map(({code,name,price})=> { 
-                    return (
-                      <div key={code}>
-                        <span>{code}</span>
-                        <span>{name}</span>
-                        <span>{price}</span>
-                      </div>
-                    )
-                  })}
-                </div>
-              </div>)
-            })
-          }</div>
-            
-        </div>
+      <Meta />
+      <Toolbar />
+      <div className="menu-body-container">
+        <main className="menu-content-container menu-page__content">
+          <div className="menu-content-block">
+            <div className="content-block__title">
+              <h2>Menu</h2>
+            </div>
+            <div className="menu-content-block__content">{
+              menu.map(({ name, items, _id }) => {
+                return (<div className="menu-group" key={_id}>
+                  <h3>{name}</h3>
+                  <div>
+                    {items.map(({ code, name, price }) => {
+                      return (
+                        <div className="menu-item" key={code}>
+                          <span className="item-code">{code}</span>
+                          <span className="item-name">{name}</span>
+                          <span className="item-blank-space"></span>
+                          <span className="item-price">{price}</span>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </div>)
+              })
+            }</div>
 
-      </main>
-      <FooterBlock />
-    </div>
-  </>
+          </div>
+
+        </main>
+        <FooterBlock />
+      </div>
+    </>
   );
 }
 
 export const getServerSideProps: GetServerSideProps<MenuPageProps> = async () => {
   const res = await fetch('https://api.milicity.eu/resto/v1/menu')
-  const {data} = await res.json();
+  const { data } = await res.json();
   return { props: { menu: data || [] } }
 }
