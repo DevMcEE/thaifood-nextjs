8c6747b1d669f471c492d4b8b3b9c962bc8e340f
Devmcee <info@devmc.ee>
2023-08-10 18:29:13 +0300

    fix pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'thaifood',
+      script: 'npm run start',
+      autorestart: true,
+    },
+  ],
+
+  deploy: {
+    production: {
+      key: '~/.ssh/id_ed25519',
+      user: 'virt98592',
+      host: 'thaifood.ee',
+      ref: 'origin/master',
+      repo: 'git@github.com:devmc-ee/thaifood-nextjs.git',
+      path: '/data01/virt98592/nodeapp/thaifood',
+      'ssh_options': 'ForwardAgent=yes',
+      'pre-deploy-local': '',
+      'post-deploy':
+        'npm install --omit=optional --force && npm run build && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': '',
+    },
+  },
+};
