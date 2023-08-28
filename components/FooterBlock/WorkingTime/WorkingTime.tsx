cb800043e0582e5c6d3d659866d8e899b520241d
Aleksei Milishenko <aleksei@nodeswat.com>
2023-08-28 16:32:12 +0300

    TE-2: fix hydration error on SSR translation

--- components/FooterBlock/WorkingTime/WorkingTime.tsx
+++ components/FooterBlock/WorkingTime/WorkingTime.tsx
@@ -1,25 +1,25 @@
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'next-i18next';
 import workingTime from '../../../assets/workingTime';
 import { WorkingTimeService } from "../../../services/workingTime.service";
 
 
-export const WorkingTime = () => {
+export const WorkingTime = (): JSX.Element => {
   const { t } = useTranslation();
+
   const workingTimeService = new WorkingTimeService(workingTime, t);
   const status = workingTimeService.getList();
-  
-  return (
-    <>
-    
-    {status.map((item) => (
-      <div key={item.weekdays} style={{display:'flex' , width:'279px', justifyContent:"space-between", textTransform:'capitalize', fontWeight:'bold'}}>
-          <p>{`${item.weekdays}: `}</p>
-          <p>{` ${item.timeRange === 'closed' ? `${item.timeRange} ${item.comment === '' ? '' :'('+item.comment+')'}` : item.timeRange}`}</p>
-      </div>
-      
-    ))}
 
-    </>
-  )
+  return (<>
+    {
+      status.map(
+        (item) => (
+          <div key={item.weekdays} style={{ display: 'flex', width: '279px', justifyContent: "space-between", textTransform: 'capitalize', fontWeight: 'bold' }}>
+            <p>{`${item.weekdays}: `}</p>
+            <p>{` ${item.timeRange === 'closed' ? `${item.timeRange} ${item.comment === '' ? '' : '(' + item.comment + ')'}` : item.timeRange}`}</p>
+          </div>
+        )
+      )
+    }
+  </>);
 }
 
