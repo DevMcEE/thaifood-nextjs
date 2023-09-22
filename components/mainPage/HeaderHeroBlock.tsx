ae6ba96457264a5c91950efbf103bf8db4b5d8e8
Aleksei Milishenko <aleksei@nodeswat.com>
2023-09-22 17:46:32 +0300

    refactor styles

--- components/mainPage/HeaderHeroBlock.tsx
+++ components/mainPage/HeaderHeroBlock.tsx
@@ -11,9 +11,8 @@ export const HeaderHeroBlock = (): JSX.Element => {
           priority={true}
           className="header-hero-block__bg-image"
           src={bgImage}
-          alt="Thai Food Thai Cook Restaurant"
+          alt="Thaiwaya Restaurant"
         />
-        <div className="header-hero-block__bg-overlay"></div>
       </div>
       <div className="header-hero-block__content">
       <div className="header-hero-block__content-title">
@@ -21,31 +20,32 @@ export const HeaderHeroBlock = (): JSX.Element => {
         <div className="header-hero-block__content-title">
         </div>
         <div className="header-hero-block__content-contacts">
-          <p className="header-hero-block__content-contacts-address">
-            <a
-              href="https://goo.gl/maps/Z5iabpug6X7DUppJ8"
-              target="_blank"
-              className="header-hero-block__content-contacts-link"
-              rel="noreferrer"
-            >
-              <Image
-                className="header-hero-block__content-contacts-icon"
-                src={mapPin}
-                alt="Thai Food Thai Cook address"
-              />
-              <span className="header-hero-block__content-contacts-address-text">Gonsiori 12a, Tallinn</span> 
-              
-            </a>
-          </p>
-          <StatusIndicator/>
-          <p className="header-hero-block__content-contacts-address">
-            <a
-              href="tel:0037258805065"
-              className="header-hero-block__content-contacts-telephone"
-            >
-              (+372) 5880 5065
-            </a>
-          </p>
+          <div className="header-hero-block__content-contacts-container">
+            <p className="header-hero-block__content-contacts-address">
+              <a
+                href="https://goo.gl/maps/Z5iabpug6X7DUppJ8"
+                target="_blank"
+                className="header-hero-block__content-contacts-link"
+                rel="noreferrer"
+              >
+                <Image
+                  className="header-hero-block__content-contacts-icon"
+                  src={mapPin}
+                  alt="Thai Food Thai Cook address"
+                />
+                <span className="header-hero-block__content-contacts-address-text">Gonsiori 12a, Tallinn</span> 
+              </a>
+            </p>
+            <StatusIndicator/>
+            <p className="header-hero-block__content-contacts-address">
+              <a
+                href="tel:0037258805065"
+                className="header-hero-block__content-contacts-telephone"
+              >
+                (+372) 5880 5065
+              </a>
+            </p>
+          </div>
         </div>
       </div>
     </header>
