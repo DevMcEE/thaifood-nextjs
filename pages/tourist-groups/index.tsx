c1d54419b885457817c40faebdee536860027cf4
Aleksei Milishenko <aleksei@nodeswat.com>
2023-10-27 00:13:08 +0300

    clean

--- pages/tourist-groups/index.tsx
+++ pages/tourist-groups/index.tsx
@@ -2,17 +2,9 @@ import { GetServerSideProps } from 'next';
 import { FooterBlock } from '../../components/FooterBlock';
 import { Meta } from '../../components/Meta';
 import { Toolbar } from '../../components/Toolbar';
-import { IMenuGroup, IMenuNavGroup } from '../../menu/menu.type';
-import { MenuGroup } from '../../menu/MenuGroup';
 import { useTranslation } from 'next-i18next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { NavigationBar } from '../../menu/MenuNavigationBar';
-import { SyntheticEvent, useCallback, useEffect, useMemo, useState } from 'react';
-import { useRefStorage } from '../../hooks/useRefStorage';
-import { SearchBar } from '../../menu/SearchBar/SearchBar';
-import { useMenuFilter } from '../../menu/hooks/useMenuFilter';
-import { MenuPlaceHolder } from '../../menu/MenuPlaceHolder';
-import Image from 'next/image';
+import { useEffect, useMemo } from 'react';
 import { Cloudinary } from '@cloudinary/url-gen';
 import { AdvancedImage, lazyload, placeholder } from '@cloudinary/react';
 import { improve } from '@cloudinary/url-gen/actions/adjust';
@@ -33,6 +25,7 @@ const prepareImage = (publicId: string, cdn: Cloudinary) => cdn.image(publicId)
 export default function TouristGroups() {
   const { t } = useTranslation();
 
+  
   const cdn = useMemo(() => {
     return new Cloudinary({
       cloud: {
@@ -44,6 +37,15 @@ export default function TouristGroups() {
   const set1 = useMemo(() => prepareImage('tourist-group-set-1', cdn), [cdn]);
 
   const set2 = useMemo(() => prepareImage('tourist-group-set-2', cdn), [cdn]);
+
+  useEffect(() => {
+    if (typeof window === 'undefined') {
+      return;
+    }
+
+    document.body.scroll({ top: 0 });
+
+  }, []);
   return (
     <>
       <Meta description={'Taiwaya: ' + t('tourist-groups.title')} />
